ad4dc8f5a2fa0e6b431c45f3fab671063dfcc946
julzcole <66046654+julzcole@users.noreply.github.com>
2024-10-02 16:19:15 -0400

    Removing references to wrapper factory

--- QAPlayground/Tests/BaseTest.cs
+++ QAPlayground/Tests/BaseTest.cs
@@ -3,7 +3,6 @@ using OpenQA.Selenium.Support.UI;
 using QAPlayground.PageObjects;
 using QAPlayground.TestingData;
 using QAPlayground.Utilities;
-using QAPlayground.WrapperFactory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
